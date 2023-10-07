40364b455cc70573087125bcaa09a7cc09a72b9e
vlooong <qinweilong0813@gmail.com>
2023-10-07 10:56:44 +0800

    fix: typo

--- composables/item.ts
+++ composables/item.ts
@@ -23,61 +23,61 @@ export const useItemStore = defineStore('item', () => {
   {
     key: 3,
     label: '小炒',
-    favorite: false,
+    favorite: true,
     location: '二食堂三楼'
   },
   {
     key: 4,
     label: '大众餐',
-    favorite: false,
+    favorite: true,
     location: '一食堂一楼'
   },
   {
     key: 5,
     label: '刘氏鸡公煲',
-    favorite: false,
+    favorite: true,
     location: '西门外'
   },
   {
     key: 6,
     label: '猪肝面',
-    favorite: false,
+    favorite: true,
     location: '一食堂一楼'
   },
   {
     key: 7,
     label: '东家卤味',
-    favotite: true,
+    favorite: true,
     location: '一食堂二楼'
   },
   {
     key: 8,
     label: '冒菜',
-    favotite: true,
+    favorite: true,
     location: '一食堂二楼'
   },
   {
     key: 9,
     label: '黄焖鸡',
-    favotite: true,
+    favorite: true,
     location: '一食堂二楼'
   },
   {
     key: 10,
     label: '老麻抄手',
-    favotite: true,
+    favorite: true,
     location: '一食堂一楼'
   },
   {
     key: 11,
     label: '烫饭',
-    favotite: true,
+    favorite: true,
     location: '一食堂一楼'
   },
   {
     key: 12,
     label: '自选饺子',
-    favotite: true,
+    favorite: true,
     location: '二食堂负一楼'
   },
   ]
